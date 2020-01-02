690dc9b6ef4095aee5d209cf657369e65e5b4b97
Cory Reed <swashcap@gmail.com>
2020-01-02 01:10:10 -0800

    refactor: adjust app styling

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,10 @@ export const App: React.FunctionComponent<AppProps> = ({
   className,
   ...rest
 }) => (
-  <div className={classNames('center mw3 pv3', className)} {...rest}>
-    <h1 className="f2 fw4 m0">Hello, World!</h1>
+  <div className={classNames('center mw7 pa3', className)} {...rest}>
+    <h1 className="f2 fw4 mb3 mt0 tc">Hello, World!</h1>
+    <p>
+      This tests <a href="https://en.wikipedia.org/wiki/HTTP/2">HTTP/2</a>.
+    </p>
   </div>
 );
