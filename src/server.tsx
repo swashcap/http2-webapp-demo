62b436b1271b1b244cbe4f6fea40451e266a1240
Cory Reed <swashcap@gmail.com>
2020-01-01 22:44:07 -0800

    refactor: use ReactDOM for full HTML template

--- src/server.tsx
+++ src/server.tsx
@@ -1,39 +1,37 @@
 import 'hard-rejection/register';
 
 import React from 'react';
-import http2 from 'http2';
-import intoStream from 'into-stream';
-import mergeStream from 'merge-stream';
-import { Readable, Writable } from 'stream';
+import http from 'http';
 import { renderToNodeStream } from 'react-dom/server';
 
 import { App } from './app';
 
 const port = process.env.PORT || 3000;
 
-const getTemplateStream = () =>
-  mergeStream(
-    intoStream(`
-  <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <title></title>
-        <meta name="viewport" content="widget=device-width, initial-scale=1">
-      </head>
-      <body>
-        <div id="app">`),
-    renderToNodeStream(<App />) as Readable,
-    intoStream(`</div>
-      </body>
-    </html>`)
-  );
-
-export const server = http2.createServer((req, res) => {
+export const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.writeHead(200, {
-    'content-type': 'text/html',
+    'Content-Type': 'text/html',
   });
-  getTemplateStream().pipe((res as unknown) as Writable);
+  res.write('<!doctype html>', () => {
+    renderToNodeStream(
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <title>HTTP2 Webapp Demo</title>
+          <meta
+            name="viewport"
+            content="widget=device-width, initial-scale=1"
+          />
+        </head>
+        <body>
+          <div id="app">
+            <App />
+          </div>
+        </body>
+      </html>
+    ).pipe(res);
+  });
 });
 
 server.listen(port, () => {
