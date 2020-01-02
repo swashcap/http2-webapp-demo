5a3f8bb1b537af39f31ac30d6670bbae3b8e1196
Cory Reed <swashcap@gmail.com>
2020-01-02 02:11:14 -0800

    refactor: change cert generation

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -2,10 +2,10 @@
 set -eo pipefail
 
 mkdir -p ssl
-openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' \
-  -keyout ssl/localhost-privkey.pem -out ssl/localhost-cert.pem
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/CN=localhost:4000' \
+  -keyout ssl/localhost.key -out ssl/localhost.crt
 
 # https://apple.stackexchange.com/a/80625
 sudo security add-trusted-cert -d -r trustRoot -k \
-  /Library/Keychains/System.keychain ssl/localhost-cert.pem
+  /Library/Keychains/System.keychain ssl/localhost.crt
 
